d78cb6aff9a7f656c91843b4e156b91a7f7f1f13
Giovanni 'CyB3rn0id' Bernardo <settorezero@gmail.com>
2020-09-08 17:22:03 +0200

    Add files via upload

---
+++ arduino/Coviclock/secrets.h
@@ -0,0 +1,13 @@
+// User/Passwords for some stuff of the Coviclock
+
+// ...for Wifi
+const char* ssid="YOUR-SSID";
+const char* password="YOUR-WIFI-PASSWORD";
+
+// ...for MQTT
+const char* mqtt_user="YOUR-MQTT-USERNAME";
+const char* mqtt_password="YOUR-MQTT-PASSWORD";
+
+// here is a tutorial for set user and password for mosquitto:
+// http://www.steves-internet-guide.com/mqtt-username-password-example/
+// user and password for MQTT server
